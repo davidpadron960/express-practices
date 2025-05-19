7e760a1e4cdc3d009329eb51f9708271deddb7ea
David Padrón <davidpadron960@gmail.com>
2025-05-19 16:17:36 -0400

    middlewares

--- index.js
+++ index.js
@@ -2,12 +2,47 @@ const express = require("express");
 
 const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({extended:false}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.post("/user",(req,res) => {
-    console.log(req.body)
-    res.send("usuario creado")
+app.get("/hello/:user",(req,res) => {
+    res.send(`Hello ${req.params.user}`)
+})
+
+app.get("/add/:x/:y",(req,res) => {
+  const {x , y} = req.params
+  const result = parseInt(x) + parseInt(y)
+  res.send(`Result: ${result}`)
+})
+
+app.get("/users/:username/photo",(req,res) => {
+ if (req.params.username === "david") {
+    return res.sendFile("./javascript.png",{
+      root:__dirname
+    })
+ }
+ res.send("El usuario no tiene acceso")
+})
+
+app.use((req,res,next) => {
+  console.log('paso por aqui')
+  next()
+})
+
+app.use((req,res,next) => {
+  if (req.query.login === 'davidpadron@gmail.com'){
+    next()
+  }else{
+    res.send('no autorizado')
+  } 
+})
+
+app.get("/dashboard",(req,res) => {
+  res.send("Dashboard page")
+})
+
+app.get("/profile",(req,res) => {
+  res.send('profile page')
 })
 
 app.listen(3000);
